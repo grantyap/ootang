52d8ba19d8b43f0014d8c226098aab7f2855490d
Grant Yap <grantivanyap@yahoo.com>
2021-05-04 01:53:11 +0800

    WIP

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,4 +2,10 @@
   import "carbon-components-svelte/css/all.css";
 </script>
 
+<svelte:head>
+  <meta property="og:title" content="Ootang" />
+  <meta property="og:descrption" content="Keep tabs on borrowed money" />
+  <title>Ootang</title>
+</svelte:head>
+
 <slot />
