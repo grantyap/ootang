2ef8a752b4095cf101cb94561fb8bc658c3db098
Grant Yap <grantivanyap@yahoo.com>
2021-05-04 02:53:34 +0800

    Add meta tags
    This makes it so it looks cooler when you share the link on Messenger B)

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -3,9 +3,33 @@
 </script>
 
 <svelte:head>
-  <meta property="og:title" content="Ootang" />
-  <meta property="og:descrption" content="Keep tabs on borrowed money" />
+  <!-- Primary Meta Tags -->
   <title>Ootang</title>
+  <meta name="title" content="Ootang – Keep tabs on borrowed money" />
+  <meta
+    name="description"
+    content="Never forget about the money your friend owes you with Ootang."
+  />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://ootang.vercel.app/" />
+  <meta property="og:title" content="Ootang – Keep tabs on borrowed money" />
+  <meta
+    property="og:description"
+    content="Never forget about the money your friend owes you with Ootang."
+  />
+  <meta property="og:image" content="/metaimage.png" />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://ootang.vercel.app/" />
+  <meta property="twitter:title" content="Ootang – Keep tabs on borrowed money" />
+  <meta
+    property="twitter:description"
+    content="Never forget about the money your friend owes you with Ootang."
+  />
+  <meta property="twitter:image" content="/metaimage.png" />
 </svelte:head>
 
 <slot />
