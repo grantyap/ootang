2dc4d91e465822b2bec6c55041fbd53ac6a28ddf
Grant Yap <grantivanyap@yahoo.com>
2021-05-03 19:49:46 +0800

    Rename types in debt API

--- src/routes/api/debt/index.json.ts
+++ src/routes/api/debt/index.json.ts
@@ -1,5 +1,5 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import type { Data } from "$lib/database";
+import type { Debt } from "$lib/database";
 
 import { getDebtsOfUser, addDebt } from "$lib/database";
 
@@ -17,7 +17,7 @@ export const get: RequestHandler = async (request) => {
 };
 
 export const post: RequestHandler = async (request) => {
-  const data = request.body.valueOf() as Data;
+  const data = request.body.valueOf() as Debt;
   addDebt(data);
 
   return {
