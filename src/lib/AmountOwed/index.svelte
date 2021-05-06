8619b99b483ca680d2739f14618ec836ecd770c9
Grant Yap <grantivanyap@yahoo.com>
2021-05-06 21:55:52 +0800

    Allow user to mark all debts to another user as paid

--- src/lib/AmountOwed/index.svelte
+++ src/lib/AmountOwed/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Button } from "carbon-components-svelte";
   import type { User, DebtWithId } from "$lib/database";
 
   export let debts: DebtWithId[];
@@ -27,31 +28,72 @@
       })
       .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   };
+
+  const markAllDebtsPaid = (userFromId: string, userToId: string) => {
+    let debtIdsToMarkAsPaid = [];
+    debts = debts.map((d) => {
+      if (
+        (d.debtor_id === userFromId && d.debtee_id === userToId) ||
+        (d.debtor_id === userToId && d.debtee_id === userFromId)
+      ) {
+        d.is_paid = true;
+        debtIdsToMarkAsPaid = [...debtIdsToMarkAsPaid, d._id];
+      }
+      return d;
+    });
+
+    debtIdsToMarkAsPaid.forEach(async (d) => {
+      await fetch(`api/debt/${d}.json?is_paid`, {
+        method: "PATCH"
+      });
+    });
+  };
 </script>
 
 {#each users.filter((u) => u._id !== currentUserId) as otherUser}
-  <p>
-    {#if totalAmountOwed(currentUserId, otherUser._id) > 0}
-      You owe <span class="bold">
-        {otherUser.name}
-      </span>
-      a total of
-      <span class="red">
-        ₱{totalAmountOwed(currentUserId, otherUser._id)}
-      </span>
-    {:else if totalAmountOwed(currentUserId, otherUser._id) < 0}
-      <span class="bold">
-        {otherUser.name}
-      </span>
-      owes you a total of
-      <span class="green">
-        ₱{totalAmountOwed(otherUser._id, currentUserId)}
-      </span>
-    {/if}
-  </p>
+  {#if totalAmountOwed(currentUserId, otherUser._id) !== 0}
+    <div class="display-flex flex-direction-row align-items-center">
+      <p>
+        {#if totalAmountOwed(currentUserId, otherUser._id) > 0}
+          You owe <span class="bold">
+            {otherUser.name}
+          </span>
+          a total of
+          <span class="red">
+            ₱{totalAmountOwed(currentUserId, otherUser._id)}
+          </span>
+        {:else if totalAmountOwed(currentUserId, otherUser._id) < 0}
+          <span class="bold">
+            {otherUser.name}
+          </span>
+          owes you a total of
+          <span class="green">
+            ₱{totalAmountOwed(otherUser._id, currentUserId)}
+          </span>
+        {/if}
+      </p>
+      <Button
+        kind="ghost"
+        size="small"
+        on:click={() => markAllDebtsPaid(currentUserId, otherUser._id)}>Mark all as paid</Button
+      >
+    </div>
+  {/if}
 {/each}
 
 <style>
+  .display-flex {
+    display: flex;
+  }
+
+  .flex-direction-row {
+    flex-direction: row;
+  }
+
+  .align-items-center {
+    align-items: center;
+  }
+
   .green {
     color: rgb(12, 158, 90);
   }
